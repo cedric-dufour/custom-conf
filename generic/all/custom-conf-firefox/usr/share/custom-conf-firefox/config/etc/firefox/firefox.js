2804a0f89ec4666cd329e602ac745cce1fabcb1a
Cédric Dufour <cedric.dufour@ced-network.net>
2019-01-16 16:52:53 +0100

    Package: generic/all/custom-conf-firefox (generic/debian/custom-conf-firefox-esr)  * Use (JavaScript-enable) auto-configuration file  - provision system-wide installation of Gecko Media Plugins (GMP)

--- generic/all/custom-conf-firefox/usr/share/custom-conf-firefox/config/etc/firefox/firefox.js
+++ generic/all/custom-conf-firefox/usr/share/custom-conf-firefox/config/etc/firefox/firefox.js
@@ -1,47 +1,7 @@
 // CUSTOM-CONF: File automatically created/updated [custom-conf-firefox]
 
-// Allow extensions update check
-pref("extensions.update.enabled", true);
-
-// Use LANG environment variable to choose locale
-pref("intl.locale.matchOS", true);
-
-// Disable default browser checking.
-pref("browser.shell.checkDefaultBrowser", false);
-
-// identify default locale to use if no /usr/lib/firefox-addons/searchplugins/LOCALE
-// exists for the current used LOCALE
-pref("distribution.searchplugins.defaultLocale", "en-GB");
-
-// Prevent EULA dialog to popup on first run
-lockPref("browser.EULA.override", true);
-
-// Prevent milestone page to popup on version change
-lockPref("browser.startup.homepage_override.mstone", "ignore");
-
-// Enable the NetworkManager integration
-lockPref("toolkit.networkmanager.disable", false);
-
-// Disable IPv6 DNS Lookup
-//pref("network.dns.disableIPv6", true);
-
-// Avoid openh264 being downloaded
-pref("media.gmp-manager.url.override", "data:text/plain,");
-
-// Disable openh264
-pref("media.gmp-gmpopenh264.enabled", false);
-
-// Default to classic view for about:newtab
-sticky_pref("browser.newtabpage.enhanced", false);
-
-// Disable health report upload
-lockPref("datareporting.healthreport.uploadEnabled", false);
-
-// Force cache management to sensible settings
-lockPref("browser.cache.disk.smart_size.enabled", false);
-lockPref("browser.cache.disk.smart_size.first_run", false);
-lockPref("browser.cache.disk.parent_directory", "~/.cache/firefox");
-lockPref("browser.cache.disk.capacity", 262144);
-lockPref("browser.cache.offline.parent_directory", "~/.cache/firefox");
-lockPref("browser.cache.offline.capacity", 262144);
+// Use (JavaScript-enabled) auto-configuration file
+// REF: https://support.mozilla.org/en-US/kb/customizing-firefox-using-autoconfig
+lockPref("general.config.filename", "custom-conf-firefox.cfg");
+lockPref("general.config.obscure_value", 0);
 
